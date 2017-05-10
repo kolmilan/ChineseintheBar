f6ce4024d8afc5dff7175ffab48caaa903c4a307
Milán Kolki <kolmilan@gmail.com>
2017-05-10 12:15:58 +0200

    GameActivity created

--- app/src/main/java/at/ac/univie/informatik/chineseinthebar/GameActivity.java
+++ app/src/main/java/at/ac/univie/informatik/chineseinthebar/GameActivity.java
@@ -3,11 +3,11 @@ package at.ac.univie.informatik.chineseinthebar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ChoosePhraseActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_choose_phrase);
+        setContentView(R.layout.activity_game);
     }
 }
