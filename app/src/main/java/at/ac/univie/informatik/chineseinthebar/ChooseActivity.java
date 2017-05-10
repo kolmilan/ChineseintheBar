a6ed0a76e7cc24aa95f6f91ae6fbd32f030b81ef
Milán Kolki <kolmilan@gmail.com>
2017-05-10 11:17:28 +0200

    ChoosePhraseActivity and ShowPhraseActivity created

---
+++ app/src/main/java/at/ac/univie/informatik/chineseinthebar/ChooseActivity.java
@@ -0,0 +1,32 @@
+package at.ac.univie.informatik.chineseinthebar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class ChooseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose);
+
+        String[] array = {"Item1", "Item2", "Item3"};
+        ListAdapter chooseAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
+        ListView chooseListView = (ListView) findViewById(R.id.chooseListView);
+        chooseListView.setAdapter(chooseAdapter);
+
+        chooseListView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                    }
+                }
+        );
+    }
+}
