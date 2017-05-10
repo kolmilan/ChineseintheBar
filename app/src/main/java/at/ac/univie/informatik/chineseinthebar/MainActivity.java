f3534c866d2d28381e1634630197281077bd25a5
Milán Kolki <kolmilan@gmail.com>
2017-05-10 18:51:14 +0200

    MainActivity buttons now take you to ChooseActivity

--- app/src/main/java/at/ac/univie/informatik/chineseinthebar/MainActivity.java
+++ app/src/main/java/at/ac/univie/informatik/chineseinthebar/MainActivity.java
@@ -2,6 +2,11 @@ package at.ac.univie.informatik.chineseinthebar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    public void eatOnClick(View view) {
+        Intent i = new Intent(this, ChooseActivity.class);
+        startActivity(i);
+    }
+
+    public void talkOnClick(View view) {
+        Intent i = new Intent(this, ChooseActivity.class);
+        startActivity(i);
+    }
+
+    public void drinkOnClick(View view) {
+        Intent i = new Intent(this, ChooseActivity.class);
+        startActivity(i);
+    }
+
+    public void danceOnClick(View view) {
+        Intent i = new Intent(this, ChooseActivity.class);
+        startActivity(i);
+    }
+
+
 }
