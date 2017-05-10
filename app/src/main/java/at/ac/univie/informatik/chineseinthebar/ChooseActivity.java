f2b12ba6c8f6059fd21e311baa71b77fd8a46e26
Milán Kolki <kolmilan@gmail.com>
2017-05-10 20:10:38 +0200

    failed attempt at getting ChooseActivity to display data based on which button was pressed

--- app/src/main/java/at/ac/univie/informatik/chineseinthebar/ChooseActivity.java
+++ app/src/main/java/at/ac/univie/informatik/chineseinthebar/ChooseActivity.java
@@ -18,10 +18,38 @@ public class ChooseActivity extends AppCompatActivity {
         Bundle mainData = getIntent().getExtras();
         if (mainData == null)
             return;
-        String mainChoice = mainData.getString("mainMessage");
+        String mainChoice = mainData.getString("sender");
 
         String[] array = {"Item1", "Item2", "Item3"};
+
+        String[] eatArray = {"Food1", "Food2", "Food3"};
+        String[] talkArray = {"Talk1", "Talk2", "Talk3"};
+        String[] drinkArray = {"Drink1", "Drink2", "Drink3"};
+        String[] danceArray = {"Dance1", "Dance2", "Dance3"};
+
+
+        /* funktioniert nicht
+        if (mainChoice == "eat") {
+            ListAdapter chooseAdapter =
+                    new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eatArray);
+        }
+        else if (mainChoice == "talk") {
+            ListAdapter chooseAdapter =
+                    new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, talkArray);
+        }
+        else if (mainChoice == "drink") {
+            ListAdapter chooseAdapter =
+                    new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drinkArray);
+        }
+        else if (mainChoice == "dance") {
+            ListAdapter chooseAdapter =
+                    new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, danceArray);
+        }
+        else {
+            return;
+        } */
         ListAdapter chooseAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, array);
+
         ListView chooseListView = (ListView) findViewById(R.id.chooseListView);
         chooseListView.setAdapter(chooseAdapter);
 
