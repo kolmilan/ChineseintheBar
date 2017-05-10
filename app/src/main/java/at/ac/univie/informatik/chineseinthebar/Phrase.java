79eb64a16865388b36d92ac3ae02a9b770f0ba8c
Milán Kolki <kolmilan@gmail.com>
2017-05-10 15:48:15 +0200

    ids added for MainActivity buttons Phrase class added

---
+++ app/src/main/java/at/ac/univie/informatik/chineseinthebar/Phrase.java
@@ -0,0 +1,12 @@
+package at.ac.univie.informatik.chineseinthebar;
+
+/**
+ * Created by milan on 10.05.17.
+ */
+
+public class Phrase {
+    String english;
+    String chinese;
+    String pinyin;
+
+}
